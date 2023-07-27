a328c21e17d1e2257c9cef23c6b9147ff88617cc
Musa Deniev <tallermusa@gmail.com>
2023-07-27 12:45:48 +0300

    Initial Commit

---
+++ TestAppMetalPetal/TestAppMetalPetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppMetalPetalApp.swift
+//  TestAppMetalPetal
+//
+//  Created by Musa on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppMetalPetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
